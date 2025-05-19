1dda9870d83f1c7e466ef895a4ff9ae11d33cfdc
rakshita-018 <rakshita1812@gmail.com>
2025-05-19 16:37:30 +0530

    express file

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>crud application</h1>
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
